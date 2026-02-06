a206d24ca11d6c912d510619f698810626ce73f6
kay-ai <favourobasi6@gmail.com>
2026-02-06 10:46:54 +0100

    add types

---
+++ types/window.d.ts
@@ -0,0 +1,23 @@
+interface StorageResult {
+  key: string
+  value: string
+  shared: boolean
+}
+
+interface ListResult {
+  keys: string[]
+  prefix?: string
+  shared: boolean
+}
+
+interface Window {
+  storage: {
+    get(key: string, shared?: boolean): Promise<StorageResult | null>
+    set(key: string, value: string, shared?: boolean): Promise<StorageResult | null>
+    delete(key: string, shared?: boolean): Promise<{ key: string; deleted: boolean; shared: boolean } | null>
+    list(prefix?: string, shared?: boolean): Promise<ListResult | null>
+  }
+  fs: {
+    readFile(filepath: string, options?: { encoding?: string }): Promise<Uint8Array | string>
+  }
+}
